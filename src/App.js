1bad5dc931cd67ed6610cbdaeeddc924371c8ecd
gadzilla143 <egor1231232@gmail.com>
2021-01-17 14:22:32 +0300

    feat: add api

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
-import React from 'react';
-import './App.scss';
+import React, {useEffect, useState } from 'react';
+import { fetchData } from './api';
+import styles from './App.module.scss';
 
-
+import { Cards, Chart, CountryPicker } from './components'
 
 function App() {
-  return (
+  const [data, setData] = useState({})
+
+
+  useEffect(() => {
     
-    <div>
+    async function fetchApi() {
+      const fetchedDate = await fetchData()
+      setData(fetchedDate)
       
+    }
+    
+    fetchApi()
+    
+  }, [])
+  
+  
+  
+ 
+
+
+  return (
+    <div className={styles.container}>
+  
+      <Cards data ={data}/>
+      <CountryPicker/>
+      <Chart/>
+
     </div>
   )
 }
